bbae0fbbd8efd67f584c155f6c689a5d5b8a00b1
Piotr Jelonek <piotrek.jelonek@icloud.com>
2021-08-18 22:55:09 +0200

    Edited Enemy Plane
    Changed Enemy Plane (added script and rigidbody, and changed

---
+++ Assets/Scripts/Enemy/EnemyPlaneManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+namespace Enemy
+{
+    public class EnemyPlaneManager : MonoBehaviour
+    {
+        public GameObject planePropeller;
+        
+        void Update()
+        {
+            planePropeller.transform.Rotate(Vector3.right * 3000 * Time.deltaTime);
+            
+            DestroyOutOfBounds();
+        }
+
+        private void DestroyOutOfBounds()
+        {
+            transform.Translate(Vector3.left * 3 * Time.deltaTime);
+
+            if (transform.position.z < -7.25f)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
