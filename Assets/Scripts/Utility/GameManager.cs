27486de072671c5c02270e985c27464676b95add
Piotr Jelonek <piotrek.jelonek@icloud.com>
2021-08-25 17:39:28 +0200

    Added GameOver button
    Now game can be restarted without exit from the game!

--- Assets/Scripts/Utility/GameManager.cs
+++ Assets/Scripts/Utility/GameManager.cs
@@ -1,22 +1,30 @@
 using Player;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace Utility
 {
     public class GameManager : MonoBehaviour
     {
+        // GUI Components
+        public TextMeshProUGUI scoreText;
+        public TextMeshProUGUI gameOverText;
+        public Button restartButton;
+        
+        // Public Variables
+        public int gameScore;
+        public bool gameRunning;
+
         // Variables
         public GameObject enemyPlanePrefab;
         public GameObject enemyHelicopterPrefab;
         public GameObject[] powerupPrefab; // Get all power ups 
-
-        public TextMeshProUGUI scoreText;
+        
         
         private PlayerController _playerController;
 
-        public int gameScore;
-        
         // Actual Code
         void Start()
         {
@@ -30,23 +38,26 @@ namespace Utility
 
         void SpawnPowerups()
         {
-            Vector3 spawnPos = new Vector3(Random.Range(-9.8f, 9.8f), 6, Random.Range(-4.5f, 4.5f));
-            int randomPowerup = Random.Range(0, powerupPrefab.Length);
-            if (_playerController.gameOver == false)
+            if (gameRunning)
             {
+                Vector3 spawnPos = new Vector3(Random.Range(-9.8f, 9.8f), 6, Random.Range(-4.5f, 4.5f));
+                int randomPowerup = Random.Range(0, powerupPrefab.Length);
                 Instantiate(powerupPrefab[randomPowerup], spawnPos, powerupPrefab[randomPowerup].transform.rotation);
             }
         }
 
         void EnemyPlaneSpawn()
         {
-            Vector3 spawnPos = new Vector3(Random.Range(-7.75f, 7.75f), 6, 8); 
-            Instantiate(enemyPlanePrefab, spawnPos, enemyPlanePrefab.transform.rotation);
+            if (gameRunning)
+            {
+                Vector3 spawnPos = new Vector3(Random.Range(-7.75f, 7.75f), 6, 8); 
+                Instantiate(enemyPlanePrefab, spawnPos, enemyPlanePrefab.transform.rotation);
+            }
         }
 
         void EnemyHelicopterSpawn()
         {
-            if (_playerController.gameOver == false)
+            if (gameRunning)
             {
                 Vector3 spawnPos = new Vector3(Random.Range(-8.5f, 8.5f), 6, 8);
                 Instantiate(enemyHelicopterPrefab, spawnPos, enemyHelicopterPrefab.transform.rotation);
@@ -57,6 +68,18 @@ namespace Utility
         {
             gameScore += scoreToAdd;
             scoreText.text = "Score: " + gameScore;
+        }
+
+        public void GameOver()
+        {
+            gameRunning = false;
+            gameOverText.gameObject.SetActive(true);
+            restartButton.gameObject.SetActive(true);
+        }
+
+        public void RestartGame()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
 }
