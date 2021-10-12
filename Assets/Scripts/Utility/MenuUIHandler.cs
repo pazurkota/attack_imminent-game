885b05f133a650f878a7570341198d652febc3f5
Piotr Jelonek <piotrek.jelonek@icloud.com>
2021-10-12 14:44:01 +0200

    Volume control
    Volume control is back!

--- Assets/Scripts/Utility/MenuUIHandler.cs
+++ Assets/Scripts/Utility/MenuUIHandler.cs
@@ -1,14 +1,44 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using Utility;
 
 public class MenuUIHandler : MonoBehaviour
 {
+    // Audio mixers
+    [SerializeField] private AudioMixer musicVol;
+    [SerializeField] private AudioMixer playerVol;
+    
+    // Sliders
+    [SerializeField] private Slider musicVolSlider;
+    [SerializeField] private Slider playerVolSlider;
+    
+    // Main menu components
     [SerializeField] private GameObject mainMenu;
     [SerializeField] private GameObject settingsMenu;
+
+    private void Start()
+    {
+        playerVolSlider.value = PlayerPrefs.GetFloat("PlayerVol", 0.75f);
+        musicVolSlider.value = PlayerPrefs.GetFloat("MusicVol", 0.75f);
+    }
+
+    public void SetPlayerVolume(float sliderValue)
+    {
+        playerVol.SetFloat("PlayerVol", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("PlayerVol", sliderValue);
+    }
+
+    public void SetMusicVolume(float sliderValue)
+    {
+        musicVol.SetFloat("MusicVol", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("MusicVol", sliderValue);
+    }
 
     public void OpenSettings()
     {
