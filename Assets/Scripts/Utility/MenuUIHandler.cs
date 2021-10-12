d30381383552414b389ebc9e292bda1f220050b9
Piotr Jelonek <piotrek.jelonek@icloud.com>
2021-10-12 14:22:53 +0200

    Script replacement
    Replaced "UIManager.cs" to "MenuUIHandler.cs"

--- Assets/Scripts/Utility/MenuUIHandler.cs
+++ Assets/Scripts/Utility/MenuUIHandler.cs
@@ -1,30 +1,14 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using Player;
-using UnityEditor;
 using UnityEngine;
+using UnityEditor;
 using UnityEngine.SceneManagement;
 using Utility;
 
-public class UIManager : MonoBehaviour
+public class MenuUIHandler : MonoBehaviour
 {
-    public static UIManager Instance;
-    
     [SerializeField] private GameObject mainMenu;
     [SerializeField] private GameObject settingsMenu;
-
-    private void Awake()
-    {
-        if (Instance != null)
-        {
-            Destroy(gameObject);
-            return;
-        }
-
-        Instance = this;
-        DontDestroyOnLoad(gameObject);
-    }
 
     public void OpenSettings()
     {
